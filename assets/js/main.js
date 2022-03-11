e7a047c578b59bebc4dd3c058c39bb7845b77355
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-03-12 00:12:52 +0300

    script: It will add the hover effect for portfolio section

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,6 +28,89 @@ $(document).ready(function(){
         $("#product-1").hide();
         $("#product").show();
     });
+
+    // product toggle
+    $("#product").click(function() {
+        $("#product").hide();
+        $("#product-1").show();
+    });
+    $("#product-1").click(function() {
+        $("#product-1").hide();
+        $("#product").show();
+    });
+    
+    // Portfolio
+    $(".work1").mouseover(function(){
+        $(".work1 .p-show").show();
+        $(".work1").addClass('tinted-image');
+    });
+    $(".work1").mouseout(function(){
+        $(".work1 .p-show").hide();
+        $(".work1").removeClass('tinted-image');
+    });
+    
+    $(".work2").mouseover(function(){
+        $(".work2 .p-show").show();
+        $(".work2").addClass('tinted-image');
+    });
+    $(".work2").mouseout(function(){
+        $(".work2 .p-show").hide();
+        $(".work2").removeClass('tinted-image');
+    });
+    
+    $(".work3").mouseover(function(){
+        $(".work3 .p-show").show();
+        $(".work3").addClass('tinted-image');
+    });
+    $(".work3").mouseout(function(){
+        $(".work3 .p-show").hide();
+        $(".work3").removeClass('tinted-image');
+    });
+    
+    $(".work4").mouseover(function(){
+        $(".work4 .p-show").show();
+        $(".work4").addClass('tinted-image');
+    });
+    $(".work4").mouseout(function(){
+        $(".work4 .p-show").hide();
+        $(".work4").removeClass('tinted-image');
+    });
+    
+    $(".work5").mouseover(function(){
+        $(".work5 .p-show").show();
+        $(".work5").addClass('tinted-image');
+    });
+    $(".work5").mouseout(function(){
+        $(".work5 .p-show").hide();
+        $(".work5").removeClass('tinted-image');
+    });
+    
+    $(".work6").mouseover(function(){
+        $(".work6 .p-show").show();
+        $(".work6").addClass('tinted-image');
+    });
+    $(".work6").mouseout(function(){
+        $(".work6 .p-show").hide();
+        $(".work6").removeClass('tinted-image');
+    });
+    
+    $(".work7").mouseover(function(){
+        $(".work7 .p-show").show();
+        $(".work7").addClass('tinted-image');
+    });
+    $(".work7").mouseout(function(){
+        $(".work7 .p-show").hide();
+        $(".work7").removeClass('tinted-image');
+    });
+    
+    $(".work8").mouseover(function(){
+        $(".work8 .p-show").show();
+        $(".work8").addClass('tinted-image');
+    });
+    $(".work8").mouseout(function(){
+        $(".work8 .p-show").hide();
+        $(".work8").removeClass('tinted-image');
+    });
     
     
 
